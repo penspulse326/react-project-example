4d6c9c9468bda65c54239539f57bd9da72ed2ee6
Shin <penspulse@gmail.com>
2026-06-15 18:01:22 +0800

    feat: add back to home link at default layout

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,10 +1,20 @@
-import { Outlet } from 'react-router';
+import { Link, Outlet, useLocation } from 'react-router';
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
     <main className="flex h-screen items-center justify-center bg-slate-100 font-sans text-slate-800">
       <div className="space-y-4 text-center">
         <Outlet />
+
+        {
+          location.pathname !== '/' && (
+            <Link to="/" className="text-slate-600 underline hover:text-slate-900">
+              Back to home
+            </Link>
+          )
+        }
       </div>
     </main>
   );
